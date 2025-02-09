54494406b65cf75f060efd32eebb2b698b48bbda
Supriya Vutukuru <v.supriyareddy25@gmail.com>
2025-02-09 16:32:16 +0530

    Resolved merge conflicts

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,5 +1,5 @@
 class JsonWebToken
-  SECRET_KEY = "6553019698684584d97e63d9e4b5123b8a8189eb520a619243663e501793c9dfe7a732f869d2c324f7a94d51173a953c028dbb395f9abe8e341ce02b5228ace2"
+  SECRET_KEY = ENV['DEV_SECRET_KEY']
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
